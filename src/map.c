00a9df19faed0d4d45c4be97b1048e95d2f6c091
Eric Lauzon <beenph@gmail.com>
2013-08-23 22:31:29 -0400

    2-1.13 Release fixes,
    Fix: fixed the presence of snort in a literal warning the users about
         the plugin beeing build without proper database support.
    
    Fix: fixed endianess return value of some error cases in
         int CheckDBVersion(DatabaseData * data)
    
    Esthetic: Rewrote a condition check in dbProcessSignatureInformation(),
              has no impact on evaluation of the if statement.
    
    Fix: compile of spo_alert_fwsam with --enable-debug and --enable-ipv6
    
    Fix: Issue where stream was not completly logged due to a mergeconf
         boolean error
    
    Fix: Add handling for presence of #v1 tag in sid-msg.map file
         (pulledpork swine flu)
    
    Fix: cache lookup (all metadata) could be wrongly interpreted
         due to substring being looked up (either already cached data
         or lookup data). Added a function to return allways the largest
         string lenght, superseed previous incomplete fix applied only for reference.
    
    Bump: build version to 333.

--- src/map.c
+++ src/map.c
@@ -722,7 +722,11 @@ int ReadSidFile(Barnyard2Config *bc)
 	    if(*index == '#')
 	    {
 		index++;
-		if( strncasecmp(index,SIDMAPV2STRING,strlen(SIDMAPV2STRING)) == 0)
+		if(strncasecmp(index,SIDMAPV1STRING,strlen(SIDMAPV1STRING)) == 0)
+		{
+		    bc->sidmap_version=SIDMAPV1;
+		}
+		else if( strncasecmp(index,SIDMAPV2STRING,strlen(SIDMAPV2STRING)) == 0)
 		{
 		    bc->sidmap_version=SIDMAPV2;
 		    continue;
